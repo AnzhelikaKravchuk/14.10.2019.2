71323ce8572e839810da05161e65f5f7f51b0b9c
Lena Krivitskaya <krivitskaya.98@gmail.com>
2019-04-26 14:35:04 +0300

    complete all tasks

--- PseudoEnumerable.Tests/EnumerableTests.cs
+++ PseudoEnumerable.Tests/EnumerableTests.cs
@@ -15,23 +15,120 @@ namespace PseudoEnumerable.Tests
         #region Filter
 
         [TestCase(new int[] { 2, 3, 4, 6 }, new int[] { 2, 4, 6 })]
+        public void Filter_IntegerArray_Modulo(int[] array, int[] expected)
+        {
+            var actual = array.Filter(x => x % 2 == 0);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 2, 3, 4, 6 }, new int[] { 4, 6 })]
         public void Filter_IntegerArray(int[] array, int[] expected)
         {
-            Func<int, bool> func = FiltersNumber;
+            var actual = array.Filter(x => x * x > 10);
 
-            var actual = array.Filter(func);
-
-            int i = 0;
-
-            foreach (var item in actual)
-            {
-                Assert.AreEqual(expected[i++], item);
-            }
+            CollectionAssert.AreEqual(expected, actual);
         }
-                
+
         [TestCase(null)]
-        public void Filter_IntegerArray_Throws_ArgumentNullException(int[] array)
-            => Assert.Throws<ArgumentNullException>(() => array.Filter(FiltersNumber));
+        public void Filter_NullArray_Throws_ArgumentNullException(int[] array)
+            => Assert.Throws<ArgumentNullException>(() => array.Filter(x => x % 2 == 0));
+
+        [TestCase(null)]
+        public void Filter_IntegerArrayAndNullFunc_Throws_ArgumentNullException(int[] array)
+           => Assert.Throws<ArgumentNullException>(() => array.Filter(null));
+        
+        #endregion
+
+        #region Transform
+
+        [TestCase(new int[] { 2, 4, 6 }, new string[] { "2", "4", "6" })]
+        public void Transform_IntegerArrayToStringArray(int[] array, string[] expected)
+        {
+            var actual = array.Transform(x => x.ToString());
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 2, 4, 6 }, new int[] { 4, 16, 36 })]
+        public void Transform_IntegerArray_DoubledIntegerArray(int[] array, int[] expected)
+        {
+            var actual = array.Transform(x => Math.Pow(x, 2));
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(null)]
+        public void Transform_NullArrayAndFunc_Throws_ArgumentNullException(int[] array)
+           => Assert.Throws<ArgumentNullException>(() => array.Transform(x => Math.Pow(x, 2)));
+
+        [TestCase(null)]
+        public void Transform_IntegerArrayAndNullFunc_Throws_ArgumentNullException(int[] array)
+          => Assert.Throws<ArgumentNullException>(() => array.Transform<int, int>(null));
+
+        #endregion
+
+        #region SortBy
+
+        [TestCase(new string[] { "abc", "ab", "abcd", "a" }, new string[] { "a", "ab", "abc", "abcd" })]
+        public void SortBy_CorrectInputStringArray(string[] source, string[] expected)
+        {
+            var actual = source.SortBy(x => x.Length);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new string[] { "abc", "ab", "abcd", "a" }, new string[] {"abcd", "abc", "ab", "a" })]
+        public void SortByDescending_CorrectInputStringArray(string[] source, string[] expected)
+        {
+            var actual = source.SortByDescending(x => x.Length);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 5, 1, 4 }, new int[] { 1, 4, 5 })]
+        public void SortBy_CorrectInputIntegerArray(int[] source, int[] expected)
+        {
+            var actual = source.SortBy(x => Math.Pow(x, 2));
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 5, 1, 4 }, new int[] { 5, 4, 1 })]
+        public void SortByDescending_CorrectInputIntegerArray(int[] source, int[] expected)
+        {
+            var actual = source.SortByDescending(x => Math.Pow(x, 2));
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 64, 48, 47 }, new char[] { '/', '0', '@' })]
+        public void SortBy_CorrectInputIntegerArray_CharArray(int[] source, char[] expected)
+        {
+            var actual = source.SortBy(x => (char)x);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 5, 16, 4, 2 }, new int[] { 2, 5, 4, 16 })]
+        public void SortBy_CorrectInputIntegerArray_(int[] source, int[] expected)
+        {
+            CustomComparer comparer = new CustomComparer();
+
+            var actual = source.SortBy(x => (int)Math.Pow(x, 2), comparer);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 5, 16, 4, 2 }, new int[] { 16, 5, 4, 2 })]
+        public void SortByDescending_CorrectInputIntegerArray_(int[] source, int[] expected)
+        {
+            CustomComparer comparer = new CustomComparer();
+
+            var actual = source.SortByDescending(x => (int)Math.Pow(x, 2), comparer);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
 
         #endregion
 
@@ -39,7 +136,7 @@ namespace PseudoEnumerable.Tests
 
         [TestCase(new int[] { 2, 8, 4, 6 }, ExpectedResult = true)]
         public bool Filter_IntegerArray_(int[] array)
-            => array.ForAll(FiltersNumber);
+            => array.ForAll(x => x % 2 == 0);
 
         #endregion
 
@@ -50,18 +147,29 @@ namespace PseudoEnumerable.Tests
         {
             ArrayList list = new ArrayList
             {
-                "ds",
+                2,
                 2
             };
 
-            Assert.Throws<InvalidCastException>(() => Enumerable.CastTo<int>(list));
+            CollectionAssert.AreEqual(new int[] { 2, 2 }, list.CastTo<int>());
         }
 
         #endregion
 
-        public bool FiltersNumber(int number)
+        #region Generator
+
+        [TestCase(2, 3)]
+        public void Generator_CorrectStartIndexAndCount_GeneratedSequence(int start, int count)
         {
-            return number % 2 == 0;
+            CollectionAssert.AreEqual(new int[] { 4, 9, 16 }, Enumerable.Generator(start, count, x => x * x));
         }
-    }       
+
+        [Test]
+        public void Generator_CorrectStartIndexAndIncorrectCount_Throws_ArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => Enumerable.Generator(4, 0, null));
+        }
+
+        #endregion
+    }
 }
--- PseudoEnumerable/Enumerable.cs
+++ PseudoEnumerable/Enumerable.cs
@@ -34,18 +34,6 @@ namespace PseudoEnumerable
             return Filter_(source, predicate);
         }
 
-        private static IEnumerable<TSource> Filter_<TSource>(IEnumerable<TSource> source,
-            Func<TSource, bool> predicate)
-        {            
-            foreach (var i in source)
-            {
-                if (predicate(i))
-                {
-                    yield return i;
-                }
-            }
-        }
-
         /// <summary>
         /// Transforms each element of a sequence into a new form.
         /// </summary>
@@ -62,7 +50,17 @@ namespace PseudoEnumerable
         public static IEnumerable<TResult> Transform<TSource, TResult>(this IEnumerable<TSource> source,
             Func<TSource, TResult> transformer)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (transformer == null)
+            {
+                throw new ArgumentNullException(nameof(transformer));
+            }
+
+            return Transform_(source, transformer);
         }
 
         /// <summary>
@@ -80,7 +78,45 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> SortBy<TSource, TKey>(this IEnumerable<TSource> source,
             Func<TSource, TKey> key)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return source.SortByKey(key, Comparer<TKey>.Default, 1);
+        }
+
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending order according to a key.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by key.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="key">A function to extract a key from an element.</param>
+        /// <returns>
+        ///     An <see cref="IEnumerable{TSource}"/> whose elements are sorted according to a key.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="key"/> is null.</exception>
+        public static IEnumerable<TSource> SortByDescending<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> key)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return source.SortByKey(key, Comparer<TKey>.Default, -1);
         }
 
         /// <summary>
@@ -100,7 +136,57 @@ namespace PseudoEnumerable
         public static IEnumerable<TSource> SortBy<TSource, TKey>(this IEnumerable<TSource> source,
             Func<TSource, TKey> key, IComparer<TKey> comparer)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            return source.SortByKey(key, comparer, 1);
+        }
+
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending order according by using a specified comparer for a key .
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <typeparam name="TKey">The type of the key returned by key.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="key">A function to extract a key from an element.</param>
+        /// <param name="comparer">An <see cref="IComparer{T}"/> to compare keys.</param>
+        /// <returns>
+        ///     An <see cref="IEnumerable{TSource}"/> whose elements are sorted according to a key.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="key"/> is null.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="comparer"/> is null.</exception>
+        public static IEnumerable<TSource> SortByDescending<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> key, IComparer<TKey> comparer)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (comparer == null)
+            {
+                throw new ArgumentNullException(nameof(comparer));
+            }
+
+            return source.SortByKey(key, comparer, -1);
         }
 
         /// <summary>
@@ -113,11 +199,16 @@ namespace PseudoEnumerable
         /// </returns>
         /// <exception cref="ArgumentNullException">Throws if <paramref name="source"/> is null.</exception>
         /// <exception cref="InvalidCastException">An element in the sequence cannot be cast to type TResult.</exception>
-        public static IEnumerable<TResult> CastTo<TResult>(IEnumerable source)
+        public static IEnumerable<TResult> CastTo<TResult>(this IEnumerable source)
         {
-            foreach(var item in source)
+            if (source == null)
             {
-                if(item is TResult)
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            foreach (var item in source)
+            {
+                if (item is TResult)
                 {
                     yield return (TResult)item;
                 }
@@ -153,6 +244,78 @@ namespace PseudoEnumerable
             return true;
         }
 
-        
+        /// <summary>
+        /// Generators the specified sequence beginning from the start element.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="count">The count.</param>
+        /// <param name="func">A function to test each element for a condition.</param>
+        /// <returns>Generated sequence according with <paramref name="func"/></returns>
+        /// <exception cref="ArgumentException">Throws if <paramref name="count"/> less than 1.</exception>
+        public static IEnumerable<int> Generator(int start, int count, Func<int, int> func)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException(nameof(count));
+            }
+
+            return Generator_(start, count, func);
+        }
+
+        #region Private methods
+
+        private static IEnumerable<TSource> SortByKey<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> key, IComparer<TKey> comparer, int reverse)
+        {
+            IList<TSource> copySource = source as IList<TSource>;
+
+            foreach (var i in copySource)
+            {
+                for (int j = 0; j < copySource.Count - 1; j++)
+                {
+                    if (reverse * comparer.Compare(key(copySource[j]), key(copySource[j + 1])) > 0)
+                    {
+                        TSource str = copySource[j];
+                        copySource[j] = copySource[j + 1];
+                        copySource[j + 1] = str;
+                    }
+                }
+            }
+
+            return copySource;
+        }
+
+        private static IEnumerable<TSource> Filter_<TSource>(IEnumerable<TSource> source,
+           Func<TSource, bool> predicate)
+        {
+            foreach (var i in source)
+            {
+                if (predicate(i))
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> Transform_<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, TResult> transformer)
+        {            
+            foreach (var item in source)
+            {
+                yield return transformer(item);
+            }
+        }
+
+        private static IEnumerable<int> Generator_(int start, int count, Func<int, int> func)
+        {
+            while (count > 0)
+            {
+                yield return func(start);
+                start++;
+                count--;
+            }
+        }
+
+        #endregion
     }
 }
